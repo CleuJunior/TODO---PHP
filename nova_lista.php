5eeea2f7966f8904a0c7e96a0dfe4d89b1678f8c
CleuJunior <cleu.junior@gmail.com>
2020-05-03 14:02:57 -0300

    first commit

---
+++ nova_lista.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+
+$tarefas = $_POST['tarefa'] ?? " ";
+$tarefas = trim($tarefas);
+
+if ($tarefas) {
+    if (file_exists('tarefa.json')) {
+        $json = file_get_contents('tarefa.json');
+        $jsonArray = json_decode($json, true);
+    } else {
+        $jsonArray = [];
+
+    }
+
+    $jsonArray[$tarefas] = ["completo" => false];
+    file_put_contents('tarefa.json', json_encode($jsonArray, JSON_PRETTY_PRINT));
+
+}
